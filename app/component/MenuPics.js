4113ce80e10d079f31663597d8431d0f2ed4c427
christianokuru <okuruchristian@gmail.com>
2024-10-13 17:10:07 +0100

    Created an array of objects to hold the food items and other properties

---
+++ app/component/MenuPics.js
@@ -0,0 +1,31 @@
+// A store for all the pictures and their detials
+
+const Products = [
+    {
+      picture: "/MenuPics/chicken.avif",
+      price: 4500,
+      name: "Barbecue Chicken"
+    },
+    {
+      picture: "/MenuPics/Untitled.jpg",
+      price: 89.99,
+      name: "Barbecue Catfish"
+    },
+    {
+      picture: "/MenuPics/Untitled.jpg",
+      price: 59.99,
+      name: "Barbecue Croaker"
+    },
+    {
+      picture: "/MenuPics/Untitled.jpg",
+      price: 79.99,
+      name: "Barbecue Tilapia"
+    },
+    {
+      picture: "/MenuPics/Untitled.jpg",
+      price: 99.99,
+      name: "Barbecue Mackerel"
+    }
+  ]
+  
+  export default Products
