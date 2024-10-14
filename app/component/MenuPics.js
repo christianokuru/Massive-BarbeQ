3b1ba3e26278d833821efa4f04e5c2c1f7e7f21d
christianokuru <okuruchristian@gmail.com>
2024-10-14 16:13:13 +0100

    Added more pictures for the food menu

--- app/component/MenuPics.js
+++ app/component/MenuPics.js
@@ -2,29 +2,76 @@
 
 const Products = [
     {
-      picture: "/MenuPics/chicken.avif",
-      price: 4500,
-      name: "Barbecue Chicken"
+      picture: "/MenuPics/chicken.jpg",
+      prop: 'Regular', 
+      price: '₦4,500',
+      prop2: 'Couple', 
+      price2: '₦8,000',
+      prop3: 'Family Box', 
+      price3: '₦16,000',
+      prop4: 'Bigi Box', 
+      price4: '₦30,000',
+      prop5: 'Chicken Tank', 
+      price5: '₦45,000',
+      name: "Chicken BBQ"
     },
     {
-      picture: "/MenuPics/Untitled.jpg",
-      price: 89.99,
-      name: "Barbecue Catfish"
+      picture: "/MenuPics/catfish.jpg",
+      prop: 'Mini', 
+      price: '₦4,500',
+      prop2: 'Mini-2', 
+      price2: '₦5,200',
+      prop3: 'Regular 1', 
+      price3: '₦6,500',
+      prop4: 'Regular 2', 
+      price4: '₦8,000',
+      prop5: 'Bigi', 
+      price5: '₦12,000',
+      name: "Catfish"
     },
     {
-      picture: "/MenuPics/Untitled.jpg",
-      price: 59.99,
-      name: "Barbecue Croaker"
+      picture: "/MenuPics/croaker.jpg",
+      prop: 'Regular 1', 
+      price: '₦6,000',
+      prop2: 'Regular 2', 
+      price2: '₦8,000',
+      prop3: 'Standard', 
+      price3: '₦10,000',
+      name: "Croaker"
     },
     {
-      picture: "/MenuPics/Untitled.jpg",
-      price: 79.99,
-      name: "Barbecue Tilapia"
+      picture: "/MenuPics/turkey.jpg",
+      prop: 'Regular', 
+      price: '₦7,000',
+      prop2: 'Standard', 
+      price2: '₦14,000',
+      prop3: 'Family', 
+      price3: '₦26,000',
+      prop4: 'Turkey Tank', 
+      price4: '₦60,000',
+      name: "Turkey"
     },
     {
-      picture: "/MenuPics/Untitled.jpg",
-      price: 99.99,
-      name: "Barbecue Mackerel"
+      picture: "/MenuPics/mullet.jpg",
+      prop: 'Regular', 
+      price: '₦5,000',
+      prop2: 'Standard', 
+      price2: '₦7,000',
+      prop3: 'Big Standard', 
+      price3: '₦9,000',
+      name: "Mullet"
+    },
+    {
+      picture: "/MenuPics/bole.jpg",
+      prop: 'Boli', 
+      price: '₦1,200',
+      prop2: 'Fried Plantain', 
+      price2: '₦1,000',
+      prop3: 'Potato Chips', 
+      price3: '₦700',
+      prop4: 'Coslaw',
+      price4: '₦700',
+      name: "Sides",
     }
   ]
   
