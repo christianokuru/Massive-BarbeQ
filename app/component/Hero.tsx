6070258aedd4410cec0aa4db8319a12b6c64b30e
christianokuru <okuruchristian@gmail.com>
2024-10-15 06:58:39 +0100

    Made the image look better

--- app/component/Hero.tsx
+++ app/component/Hero.tsx
@@ -5,17 +5,17 @@ export default function Hero() {
     return(
         <div>
             <div>
-                <h1 className="text-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pt-5 pb-3 text-[#D84315]">
+                <h1 className="text-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pt-5 pb-3 text-[#D84315] px-4">
                     The <span className="text-[#5D4037]">Smokey</span> Goodness Awaits You!
                 </h1>
             </div>
             {/* subheading */}
-            <p className="text-gray-700 text-center leading-7 [&:not(:first-child)]:mt-0">
+            <p className="text-gray-700 text-center leading-7 [&:not(:first-child)]:mt-0 px-4">
                 Enjoy the rich flavors of traditional BBQ, prepared with care, just for you!
             </p>
             {/* image */}
             <div className="py-5">
-                <Image src={HeroImaage} alt="Hero_image" height={450} width={450} className="rounded-md border-4 border-[#212121]"/>
+                <Image src={HeroImaage} alt="Hero_image" height={450} width={450} className="border-t-2 border-b-2 border-[#D84315]"/>
             </div>
         </div>
     )
